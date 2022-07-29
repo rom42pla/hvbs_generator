61c0b8b6698ec6096d2d49c566b25ea38fb97915
rom42pla <romeo.lanzino@gmail.com>
2022-07-29 18:24:06 +0200

    improved memory usage

--- generator.py
+++ generator.py
@@ -1,45 +1,53 @@
 import asyncio
+import json
 import threading
-from os.path import exists
+from os.path import exists, isdir
 
-import uvicorn
-from fastapi import FastAPI
 from aiogram import Bot, Dispatcher, executor, types
-
-from data.db import Database
-from apis.routers import items
-from telegram_bot.commands import add_telegram_commands
+from transformers import pipeline
 
 
 class Generator:
-    def __init__(self, endpoints_path: str,
-                 telegram_bot_token: str):
-        assert exists(endpoints_path)
-        self.endpoints_path = endpoints_path
-        self.db = Database(endpoints_path=self.endpoints_path)
+    def __init__(self,
+                 telegram_bot_token: str,
+                 model_path: str):
+        assert isinstance(model_path, str) and isdir(model_path)
+        self.model_path = model_path
 
-        # APIs setup
-        self.apis_app = FastAPI()
-        for router, prefix in [
-            (items.get_router(generator=self), "items"),
-        ]:
-            self.apis_app.include_router(
-                router,
-                prefix=f"/{prefix}",
-                tags=[prefix],
-            )
-
-        def run_apis():
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            uvicorn.run(self.apis_app)
-
-        self.apis_thread = threading.Thread(target=run_apis)
+        # model setup
+        self.generator = pipeline(
+            "text-generation",
+            model=self.model_path,
+            tokenizer=self.model_path,
+        )
 
         # Telegram bot setup
         self.telegram_bot = Bot(token=telegram_bot_token)
         self.telegram_dispatcher = Dispatcher(self.telegram_bot)
-        add_telegram_commands(self.telegram_dispatcher)
+
+        @self.telegram_dispatcher.message_handler(commands=['start', 'help'])
+        async def send_welcome(message: types.Message):
+            await message.reply("Succhiami il cazzo.")
+
+        @self.telegram_dispatcher.message_handler(commands=["genera"])
+        async def send_welcome(message: types.Message):
+            message.text = message.text.replace("/genera", "", 1)
+            numbers = {
+                0: "0️⃣",
+                1: "1️⃣",
+                2: "2️⃣",
+                3: "3️⃣",
+                4: "4️⃣",
+                5: "5️⃣",
+                6: "6️⃣",
+                7: "7️⃣",
+                8: "8️⃣",
+                9: "9️⃣",
+            }
+            out = ""
+            for i_sentence, sentence in enumerate(self.generate_sentences(input_string=message.text, amount=4)):
+                out += f"{numbers[i_sentence + 1]}\t{sentence}\n\n"
+            await message.answer(out, disable_web_page_preview=True)
 
         def run_telegram_bot():
             loop = asyncio.new_event_loop()
@@ -48,8 +56,22 @@ class Generator:
 
         self.telegram_bot_thread = threading.Thread(target=run_telegram_bot)
 
-    def start_apis(self):
-        self.apis_thread.start()
-
     def start_telegram_bot(self):
         self.telegram_bot_thread.start()
+
+    def generate_sentences(self, input_string: str = "", amount: int = 1):
+        assert isinstance(amount, int) and amount >= 1
+        sentences = []
+        for sentence in [s["generated_text"].strip()
+                         for s in self.generator(input_string,
+                                                 do_sample=True,
+                                                 top_k=50,
+                                                 top_p=0.95,
+                                                 max_length=64,
+                                                 early_stopping=True,
+                                                 num_return_sequences=amount)]:
+            sentence = sentence.replace("\\n", "")
+            sentence = sentence.replace("\\t", "")
+            sentence = sentence.replace("  ", "")
+            sentences += [sentence]
+        return sentences
--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from generator import Generator
 
 DATA_PATH = join(".", "data")
 ENDPOINTS_JSON_PATH = join(DATA_PATH, "endpoints.json")
+MODEL_PATH = join(DATA_PATH, "model")
 
-g = Generator(endpoints_path=ENDPOINTS_JSON_PATH,
-              telegram_bot_token="5337038203:AAFfCbjG2gdbQTmAsjykxpWV41iehpLyA-k")
+g = Generator(telegram_bot_token="5337038203:AAFfCbjG2gdbQTmAsjykxpWV41iehpLyA-k",
+              model_path=MODEL_PATH)
 g.start_telegram_bot()
-g.start_apis()
